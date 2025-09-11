02c35321863acb68a29155d7dc8efc88ca030ccf
hateNaldoz <naguilar2@my.bcit.ca>
2025-09-10 23:50:34 -0700

    sb?

--- src/client/components/home_page/join_chat.tsx
+++ src/client/components/home_page/join_chat.tsx
@@ -2,15 +2,20 @@ import {useState, useEffect} from "react";
 import {MenuSetter} from "../../types/menu_setter";
 
 export default function JoinChat({setMenu}: {setMenu: MenuSetter}) {
+    const [chatrooms, setChatrooms] = useState<String[]>([]);
+
     const getChatrooms = async () => {
         const response = await fetch(`http://localhost:${import.meta.env.VITE_API_PORT}/api/chatrooms`);
         if (!response.ok) console.error("U SOLD FAM");
 
         const data = await response.json();
-        console.log(data.chatrooms);
+        setChatrooms(data.chatrooms || []);
+        // return data.chatrooms || [];
     };
 
-    useEffect(() => {}, []);
+    useEffect(() => {
+        getChatrooms();
+    }, []);
 
     return (
         <>
@@ -28,7 +33,7 @@ export default function JoinChat({setMenu}: {setMenu: MenuSetter}) {
                 </div>
                 <h3 className="text-2xl">Join an open chat.</h3>
                 <div className="border-1 border-white m-auto w-[80%]">
-                    <button onClick={() => getChatrooms()}>bruhhhhhhh</button>
+                    <button className="p-2 rounded-2xl border-1 border-black">button</button>
                 </div>
             </div>
         </>
--- src/server/api/chatrooms.ts
+++ src/server/api/chatrooms.ts
@@ -4,13 +4,19 @@ export const chatroomsApi = express.Router();
 
 chatroomsApi.get("/", async (req, res) => {
     try {
-        const chatrooms = await client.sMembers("chatrooms");
-        if (!chatrooms) return res.status(200).json({success: true, message: "No chats currently available..."});
-        console.log(chatrooms);
+        let chatroomsData = await client.sMembers("chatrooms");
+        if (!chatroomsData) return res.status(200).json({success: true, message: "No chats currently available..."});
+        console.log(chatroomsData);
 
         // testing a way to find specific rooms out of the list (ignore);
-        const chatroom = chatrooms.find((room) => room.includes("3983"));
-        console.log(chatroom);
+        /* const chatroom = chatrooms.find((room) => room.includes("3983"));
+        console.log(chatroom); */
+
+        const chatrooms = chatroomsData.map((room) => {
+            return room.slice(-4, -1);
+        });
+
+        console.log(chatrooms);
 
         return res.status(200).json({success: true, chatrooms: chatrooms});
     } catch (e) {
