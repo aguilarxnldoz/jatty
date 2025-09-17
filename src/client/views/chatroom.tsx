437e35c873ff60266c3ef3edf6b49553bc60abe9
Nald Ozem Aguilar <naldoz@naldouche.local>
2025-09-16 22:29:01 -0700

    i understand it now

--- src/client/views/chatroom.tsx
+++ src/client/views/chatroom.tsx
@@ -1,8 +1,15 @@
 import Header from "../components/header";
 import {useParams} from "react-router-dom";
+import {useRef, useEffect} from "react";
+import io from "socket.io-client";
+const socket = io.connect(`http://localhost:${import.meta.env.VITE_API_PORT}`);
 
 export default function ChatRoom() {
     const {roomId} = useParams<string>();
+
+    useEffect(() => {
+        socket.emit("roomJoined", {message: `User: ${socket.id} has joined the chatroom`});
+    });
 
     return (
         <>
