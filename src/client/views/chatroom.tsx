595f4596b69c699ba1784a9506f14499d2783bdb
Nald Ozem Aguilar <naldoz@naldouche.local>
2025-09-18 20:42:27 -0700

    add more ui shi to chatroom

--- src/client/views/chatroom.tsx
+++ src/client/views/chatroom.tsx
@@ -1,11 +1,8 @@
 import Header from "../components/header";
-import {useParams} from "react-router-dom";
+import {useParams, useNavigate} from "react-router-dom";
 import {useRef, useEffect, useState} from "react";
 import io from "socket.io-client";
 import IChatroom from "../../../public/types/IChatroom";
-import {useNavigate} from "react-router-dom";
-
-// const socket = io.connect(`http://localhost:${import.meta.env.VITE_API_PORT}`);
 
 export default function ChatRoom() {
     const navigate = useNavigate();
@@ -41,9 +38,48 @@ export default function ChatRoom() {
 
     return (
         <>
-            <div id="app-wrapper">
+            <div
+                id="app-wrapper"
+                className="min-h-screen flex flex-col"
+            >
+                <h2 className="inline text-xs  lg:text-lg">
+                    Chatroom: {chatroomData?.chatroomName}
+                    <span className="inline ml-5">Room Id: {chatroomData?.roomId}</span>
+                </h2>
                 <Header />
-                <h2>Chatroom: {chatroomData?.chatroomName}</h2>
+
+                <div
+                    id="chat-container"
+                    className="flex-1 overflow-y-auto"
+                ></div>
+
+                <div
+                    id="message-bar"
+                    className="w-full bg-white rounded-2xl p-2 sticky bottom-3"
+                >
+                    <form
+                        action=""
+                        className="w-full flex flex-row space-x-full"
+                    >
+                        <div className="flex-1">
+                            <input
+                                type="text"
+                                className="w-full p-3 text-black"
+                                placeholder="send a message"
+                            />
+                        </div>
+
+                        <div
+                            id="send-message-button"
+                            className="text-black rounded-full text-xs p-1"
+                        >
+                            <input
+                                type="submit"
+                                placeholder="->"
+                            />
+                        </div>
+                    </form>
+                </div>
             </div>
         </>
     );
