2f298c3317c55dd16c8cdd23692c02ec4b471c21
Nald Ozem Aguilar <naldoz@naldouche.local>
2025-09-21 21:33:20 -0700

    deploy commit?

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,4 +1,4 @@
-import {Routes, Route} from "react-router-dom";
+import {Routes, Route, Navigate} from "react-router-dom";
 import Home from "./views/home";
 import ChatRoom from "./views/chatroom";
 
@@ -7,12 +7,30 @@ function App() {
         <>
             <Routes>
                 <Route
+                    path="/"
+                    element={
+                        <Navigate
+                            to="/jatty/home"
+                            replace
+                        />
+                    }
+                />
+                <Route
                     path="/jatty/home"
                     element={<Home />}
                 />
                 <Route
                     path="/jatty/chatroom/:roomId"
                     element={<ChatRoom />}
+                />
+                <Route
+                    path="*"
+                    element={
+                        <Navigate
+                            to="/jatty/home"
+                            replace
+                        />
+                    }
                 />
             </Routes>
         </>
